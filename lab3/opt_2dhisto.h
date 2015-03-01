3db5e5c5298e28861b37006eaf4a5c4948d9e33c
SteveShaw <steve.shao@yahoo.com>
2015-03-02 01:59:39 +0800

    Fix bugs

--- lab3/opt_2dhisto.h
+++ lab3/opt_2dhisto.h
@@ -1,10 +1,10 @@
 #ifndef OPT_KERNEL
 #define OPT_KERNEL
 
-void opt_init(unsigned int** h_Data, int width, int height);
-void opt_2dhisto(int size);
-void opt_copyFromDevice(unsigned int* h_Histogoram);
-void opt_free();
+extern "C" void opt_init(unsigned int** h_Data, int width, int height);
+extern "C" void opt_2dhisto(int size);
+extern "C" void opt_copyFromDevice(unsigned char* output);
+extern "C" void opt_free();
 
 
 #endif
--- lab3/test_harness.cpp
+++ lab3/test_harness.cpp
@@ -89,7 +89,6 @@ int main(int argc, char* argv[])
 
     /* Include your setup code below (temp variables, function calls, etc.) */
 
-	uint32_t* gpu_bins = (uint32_t*)malloc(HISTO_HEIGHT*HISTO_WIDTH*sizeof(uint32_t));
 	opt_init(input,INPUT_WIDTH,INPUT_HEIGHT);
 
 
@@ -98,17 +97,13 @@ int main(int argc, char* argv[])
     /* This is the call you will use to time your parallel implementation */
     TIME_IT("opt_2dhisto",
             1000,
-            opt_2dhisto( HISTO_HEIGHT*HISTO_WIDTH);;)
+            opt_2dhisto( INPUT_WIDTH*INPUT_HEIGHT);)
 
     /* Include your teardown code below (temporary variables, function calls, etc.) */
 
-	opt_copyFromDevice(gpu_bins);
+	opt_copyFromDevice(kernel_bins);
 	opt_free();
-	for (int i=0; i < HISTO_HEIGHT*HISTO_WIDTH; i++)
-	{
-		if(gpu_bins[i]>255) kernel_bins[i]=255;
-		else kernel_bins[i] = gpu_bins[i];
-	}
+
     /* End of teardown code */
 
     int passed=1;
